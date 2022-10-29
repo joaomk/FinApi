ce0de815fb3c1d99cf416ab4f12beb609a9d1ee6
João Vitor da Silva <joaomk@live.com>
2022-10-29 16:39:41 -0300

    deposit function and check the existence of the account in the middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Customer not found' });
+  }
+
+  req.customer = customer;
+
+  return next();
+
+
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -29,12 +46,30 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params;
+app.get("/statement/", verifyIfExistsAccountCPF, (req, res) => {
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
+  const { customer } = req;
 
   return res.json(customer.statement);
 });
+
+app.post("/deposit/", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    create_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return res.status(200).send();
+});
+
+
 
 app.listen(3333);
